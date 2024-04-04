aa885d8bb036a902dd034546b263ac31bdde8256
S K Sricharan <sksricharan2003@gmail.com>
2024-04-04 22:41:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var restart = document.querySelector('#res');
+var squares = document.querySelectorAll('td');
+function clearBox() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+}
+restart.addEventListener('click',clearBox);
+
+function ChangeOptions() {
+  if(this.textContent === ''){
+    this.textContent = 'X';
+  }
+  else if (this.textContent ==='X') {
+    this.textContent = 'O';
+  }
+  else {
+    this.textContent = '';
+  }
+}
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',ChangeOptions);
+}
